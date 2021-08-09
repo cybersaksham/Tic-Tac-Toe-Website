2cecfe11cc28c72641e4c4d1934b3b7041cf1fdd
Saksham <2020UCP1801@mnit.ac.in>
2021-08-09 19:48:12 +0530

    modal forms added

--- static/index.js
+++ static/index.js
@@ -1,3 +1,33 @@
+function showError($id, $msg){
+    $($id).empty();
+    $($id).append($msg);
+}
+
 $(document).ready(function(){
-    console.log("Start");
+    // Pressing create room
+    $('#createRoom').click(function(e){
+        e.preventDefault();
+        if($('#createName').val().length < 4 ||
+        $('#createName').val().length > 10){
+            showError('#errorTextCreate', "Name must be between 4 & 10 characters");
+        }
+        else{
+            showError('#errorTextCreate', "")
+        }
+    });
+
+    // Pressing join room
+    $('#joinRoom').click(function(e){
+        e.preventDefault();
+        if($('#joinName').val().length < 4 ||
+        $('#joinName').val().length > 10){
+            showError('#errorTextJoin', "Name must be between 4 & 10 characters");
+        }
+        else if($('#joinID').val() == ""){
+            showError('#errorTextJoin', "Enter a room ID.");
+        }
+        else{
+            showError('#errorTextJoin', "")
+        }
+    });
 });
