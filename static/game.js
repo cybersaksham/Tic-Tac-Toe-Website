4e8213b6f6e94a1d1834d4ea570838235a5ff246
Saksham <2020UCP1801@mnit.ac.in>
2021-08-10 23:20:30 +0530

    sockeio bug fixed

--- static/game.js
+++ static/game.js
@@ -50,7 +50,7 @@ function updateHTMl($boxes, $playerID, $status, $turn){
 // Starting Point
 $(document).ready(function(){
     // Making Socket
-    $socket = io();
+    $socket = io(window.location.href);
 
     // Initializing variables
     $boxes = $('.gameBoxes');
